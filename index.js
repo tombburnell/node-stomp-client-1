cfe5406893476b5e817ac4cc8082f2cb9d5fc941
Chris Flook <chris@bubblebubble.me>
2015-01-20 16:34:33 +0000

    Add unsubscribe & event-subscriptions

--- index.js
+++ index.js
@@ -25,10 +25,19 @@ var client = create({port: 61623}, function () {
   console.info('Connected on STOMP.');
   setInterval(function() {
     client.publish('/queue/a', JSON.stringify({ prop: 'ZOMG An object', time: new Date() }));
-  }, 5000);
+  }, 3000);
 
 
-  client.subscribe('/queue/a', function (msg) {
+  var first = client.subscribe('/queue/a', function (msg) {
     console.log('Received a msg', JSON.parse(msg));
   });
+
+  var second = client.subscribe('/queue/a', function (msg) {
+    console.log('Got the msg on 2nd subscription, unsubcribing. id', second.id);
+    client.unsubscribe(second);
+  });
+
+  var third = client.subscribeToEvent(first, function (msg) {
+    console.log('Fires everytime the original does.');
+  })
 });
